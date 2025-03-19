a30c13e7767905a1225cb4905bf84a72fc571a81
gganb <gksql0801@naver.com>
2025-03-19 21:29:57 +0900

    Feat : Memo 객체를 받는 생성자 추가

--- src/main/java/com/example/memo/dto/MemoResponseDto.java
+++ src/main/java/com/example/memo/dto/MemoResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.memo.dto;
 
+import com.example.memo.entity.Memo;
 import lombok.Getter;
 
 // client에 데이터를 반환할 때 사용할 클래스
@@ -8,5 +9,12 @@ public class MemoResponseDto {
     private Long id;
     private String title;
     private String contents;
+
+    public MemoResponseDto(Memo memo){
+        this.id = memo.getId();
+        this.title = memo.getTitle();
+        this.contents = memo.getContent();
+    }
+
 
 }
