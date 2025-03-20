68814fc5cc8ec2485b16898a40e037d13f567d67
gganb <gksql0801@naver.com>
2025-03-20 11:28:58 +0900

    Fix : 업데이트 기능 추가

--- src/main/java/com/example/memo/entity/Memo.java
+++ src/main/java/com/example/memo/entity/Memo.java
@@ -1,13 +1,19 @@
 package com.example.memo.entity;
 
+import com.example.memo.dto.MemoRequestDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class Memo {
-    private Long id;
+    private Long id;    // 식별자, 서버에서 관리
     private String title;
-    private String content;
+    private String contents;
 
+
+    public void update(MemoRequestDto memoRequestDto) {
+        this.title = memoRequestDto.getTitle();
+        this.contents = memoRequestDto.getContents();
+    }
 }
