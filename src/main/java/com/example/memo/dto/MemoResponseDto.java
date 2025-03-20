24eedbbf0a610f5f2713fc45b4463746abc9e999
gganb <gksql0801@naver.com>
2025-03-20 11:29:51 +0900

    Fix : 주석 수정

--- src/main/java/com/example/memo/dto/MemoResponseDto.java
+++ src/main/java/com/example/memo/dto/MemoResponseDto.java
@@ -4,16 +4,17 @@ import com.example.memo.entity.Memo;
 import lombok.Getter;
 
 // client에 데이터를 반환할 때 사용할 클래스
+// 응답
 @Getter
 public class MemoResponseDto {
-    private Long id;
+    private Long id;    // 필요에 따라 포함
     private String title;
     private String contents;
 
     public MemoResponseDto(Memo memo){
         this.id = memo.getId();
         this.title = memo.getTitle();
-        this.contents = memo.getContent();
+        this.contents = memo.getContents();
     }
 
 
