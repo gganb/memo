be345723cb882096ef332652fa7d41895585b1ae
gganb <gksql0801@naver.com>
2025-03-20 11:29:17 +0900

    Fix : 업데이트, 삭제 기능 추가

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -3,10 +3,7 @@ package com.example.memo.controller;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entity.Memo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -20,8 +17,9 @@ public class MemoController {
     // DB 역할 수행
     private final Map<Long, Memo> memoList = new HashMap<>();
 
+    // 글 작성
     @PostMapping
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto){
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
 
         // 식별자 1씩 증가
         Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
@@ -29,8 +27,37 @@ public class MemoController {
         // 요청받은 데이터로 Memo 객체 생성
         Memo memo = new Memo(memoId, requestDto.getTitle(), requestDto.getContents());
 
-        memoList.put(memoId,memo);
+        // 메모 객체 생성
+        memoList.put(memoId, memo);
+
+        // 요청 > 응답
+        return new MemoResponseDto(memo);
+    }
+
+    // 조회
+    @GetMapping("/{id}")
+    public MemoResponseDto findMemoById(@PathVariable Long id) {
+
+        Memo memo = memoList.get(id);
 
         return new MemoResponseDto(memo);
     }
+
+    @PutMapping("/{id}")
+    public MemoResponseDto updateMemoById(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto requestDto) {
+
+        Memo memo = memoList.get(id);
+
+        memo.update(requestDto);
+
+        return new MemoResponseDto(memo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMemoById(@PathVariable Long id) {
+        memoList.remove(id);
+    }
+
 }
